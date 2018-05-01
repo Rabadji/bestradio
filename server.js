3831549776646980c3ea90d70a374a3e944e310b
Unknown <timur.rabadji@gmail.com>
2018-05-01 16:22:10 +0300

    v9

--- server.js
+++ server.js
@@ -1,24 +1,59 @@
-var express = require('express');
-var app = express();
+const express=require('express');
+const bodyParser=require("body-parser");
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extened:true}));
+const MongoClient=require("mongodb").MongoClient;
+const ObjectID=require('mongodb').ObjectID;
 var path    = require("path");
-app.use(express.static("public"));
+app.use(express.static("radio"));
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
-
 app.get('/style.css', function(req, res) {
-  res.sendFile(__dirname + "/public/" + "style.css");
+  res.sendFile(__dirname + "/radio/" + "style.css");
 });
 app.get('/page1',function(req,res){
-  res.sendFile(path.join(__dirname+'/public/page1.html'));
+  res.sendFile(path.join(__dirname+'/radio/page1.html'));
 });
 app.get('/page2',function(req,res){
-  res.sendFile(path.join(__dirname+'/public/page2.html'));
-});
-app.get('/radio/purchase',function(req,res){
-  res.sendFile(path.join(__dirname+'/public/page1.html'));
+  res.sendFile(path.join(__dirname+'/radio/page2.html'));
 });
 
-app.listen(3001, function () {
-  console.log('Example app listening on port 3000!');
+app.get('/purchase',function(req,res){
+  res.sendFile(path.join(__dirname+'/radio/page1.html'));
 });
+
+
+//####################################text####################################
+
+app.get('/text',function(req,res){
+    db.collection('text').find().toArray(function(err,docs){
+        if (err){
+            console.log(err);
+            return res.sendStatus(500);
+        }
+        res.send(docs)
+    })
+})
+
+app.post('/text',function(req,res){
+        var txt={ text:req.body.text,     
+        };
+    db.collection('text').insert(txt,function(err,result){
+        if(err){
+            console.log(err);
+            res.sendStatus(500);
+        }
+    res.send(txt);
+    })
+})
+
+MongoClient.connect("mongodb://localhost:27017/radio",function(err,database){
+    if(err){
+        return console.log(err);
+    }
+    db=database;
+    app.listen(3001);
+    
+})
